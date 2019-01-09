f20c1b4edc8e3a8b085afc5612de4bc7bd407bf7
Kazuito <kazit.kazuito@gmail.com>
2019-01-09 09:47:52 +0900

    Replace link.sh with install.sh

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+ANYENV_ROOT=$HOME/.anyenv
+
+# set up anyenv
+function setup_anyenv {
+  git clone https://github.com/riywo/anyenv.git $ANYENV_ROOT
+  mkdir $ANYENV_ROOT/plugins
+  git clone https://github.com/znz/anyenv-update.git $ANYENV_ROOT/plugins/anyenv-update
+}
+
+function make_symlink {
+  local target_files=(.bashrc .gitconfig .gitignore_global .tmux.conf .vimrc .zshrc)
+  local from_dir="$(dirname $0)"
+  local to_dir="$HOME"
+
+  for f in "${target_files[@]}"; do
+    from="$from_dir/$f"
+    to="$to_dir/$f"
+    if [ -h "$to" ] ; then
+      rm -f "$to"
+    elif [ -e "$to" ] ; then
+      mv $to "${to}.old"
+      echo "Backed up $to"
+    fi
+    ln -s $from $to
+  done
+}
+
+make_symlink
+setup_anyenv
