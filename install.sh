c5c6e62e95870bec67c4efb25d2ce6fa7f955318
Kazuito <kazit.kazuito@gmail.com>
2019-04-07 00:55:46 +0900

    Add anyenv initialization command

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ function setup_anyenv {
   git clone https://github.com/riywo/anyenv.git $ANYENV_ROOT
   mkdir $ANYENV_ROOT/plugins
   git clone https://github.com/znz/anyenv-update.git $ANYENV_ROOT/plugins/anyenv-update
+  $ANYENV_ROOT/bin/anyenv install --init
 }
 
 function make_symlink {
